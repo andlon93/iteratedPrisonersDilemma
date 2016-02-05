1ea14801433ffbf55a0c2c0d26fed4fdaae85858
olehalvor <olli408@hotmail.com>
2016-02-05 10:50:41 +0100

    Tournament methods done

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,14 @@ public class Main {
 
     }
 
-    private double[] playMatches(Agent a1, Agent a2, int numberOfRounds){
+    private double[] playMatches(Agent a1, Agent a2, int rounds){
         ArrayList<Agent.Action> a1Moves = new ArrayList<Agent.Action>();
         ArrayList<Agent.Action> a2Moves = new ArrayList<Agent.Action>();
 
         int a1Payoff = 0;
         int a2Payoff = 0;
 
-        for (int i=0; i<numberOfRounds; i++){
+        for (int i=0; i<rounds; i++){
             Agent.Action a1Move = a1.dilemma(a2Moves);
             Agent.Action a2Move = a2.dilemma(a1Moves);
             if (a1Move.equals(Agent.Action.DEFECT)){
@@ -35,13 +35,25 @@ public class Main {
             a1Moves.add(a1Move);
             a2Moves.add(a2Move);
         }
-        double[] scores = {a1Payoff/numberOfRounds,a2Payoff/numberOfRounds};
+        double[] scores = {a1Payoff/rounds,a2Payoff/rounds};
         return scores;
     }
 
-    private void playTournament(Agent[] agents){
-        for (Agent a1: agents){
+    private void playTournament(Agent[] agents, int rounds){
+        double[] totalScores = new double[agents.length];
+        double[] F_SCORE = new double[agents.length];
 
+        for (int i=0; i<agents.length; i++){
+            for (int k=0; k<agents.length; k++){
+                if (!agents[i].equals(agents[k])){
+                    double[] tempScores = playMatches(agents[i],agents[k],20);
+                    totalScores[i] += tempScores[0];
+                    totalScores[k] += tempScores[1];
+                }
+            }
+        }
+        for (int i=0; i<agents.length; i++){
+            F_SCORE[i] = totalScores[i]/5;
         }
 
     }
